026f2946b4299a027068aaf54978455724f7080e
1123ken <red_00.1123ken@live.jp>
2024-02-20 14:28:32 +0900

    コントローラの設定まで

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>お問い合わせフォーム</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
