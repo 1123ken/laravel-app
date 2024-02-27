051fbdc861b08bc8b04e344c1dcc7d35e25ab4b1
1123ken <red_00.1123ken@live.jp>
2024-02-27 11:10:17 +0900

    マイグレーション、モデル作成

---
+++ app/Models/Contact.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Contact extends Model
+{
+    use HasFactory;
+
+    //指定したカラム　更新の許可
+    protected $fillable = [
+        'email',
+        'body',
+    ];
+}
